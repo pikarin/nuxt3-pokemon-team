032b08da198a539bdab1a16c51d0be0875d0023e
Muhammad Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-12-04 21:17:53 +0700

    Add menu header component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,11 +1,31 @@
+<script lang="ts" setup>
+import { MenuIcon, PlusCircleIcon } from '@heroicons/vue/outline'
+
+const show = ref(false)
+
+const toggle = () => {
+  show.value = !show.value
+}
+</script>
+
 <template>
   <header class="shadow">
     <div class="container py-4">
       <nav class="flex items-center justify-between">
-        <div />
-        <span class="text-blue-500 text-2xl">Pokemon</span>
-        <div />
+        <div class="w-10">
+          <button @click="toggle">
+            <MenuIcon class="h-6 w-6" />
+          </button>
+        </div>
+        <span class="text-blue-500 text-2xl">Pokémon</span>
+        <div class="w-10">
+          <button class="text-blue-500">
+            <PlusCircleIcon class="h-8 w-8" />
+          </button>
+        </div>
       </nav>
     </div>
+
+    <AppMenu v-if="show" @close="toggle" />
   </header>
 </template>
---
+++ components/AppMenu.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+interface NavMenu {
+  label: string
+  url: string
+}
+
+defineEmits(['close'])
+
+const navMenus: NavMenu[] = [
+  {
+    label: 'Teams',
+    url: '/teams',
+  },
+  {
+    label: 'My Favorite Pokemon',
+    url: '/favourites',
+  },
+]
+</script>
+
+<template>
+  <nav class="border-t border-gray-100 py-4">
+    <ul>
+      <li v-for="(menu, index) in navMenus" :key="index">
+        <a
+          href="#"
+          class="block p-2 mx-2 border border-transparent hover:border-blue-300 rounded-md"
+          @click="$emit('close')"
+        >
+          {{ menu.label }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
