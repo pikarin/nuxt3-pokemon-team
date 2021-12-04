bbe397160c0a0ca03a034c4e41d60dfdd49c3bc4
Muhammad Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-12-05 05:59:46 +0700

    add PokemonList component

---
+++ components/PokemonList.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const { POKE_API_URL } = useRuntimeConfig()
+const props = defineProps({
+  limit: {
+    type: Number,
+    default: 20,
+  },
+})
+
+const randomOffset = () => Math.floor(Math.random() * 1000)
+
+const { data }: { data: any } = await useFetch(
+  `${POKE_API_URL}/pokemon?limit=${props.limit}&offset=${randomOffset()}`
+)
+</script>
+
+<template>
+  <ul>
+    <li v-for="pokemon in data.results" :key="pokemon.name">
+      {{ pokemon.name }}
+    </li>
+  </ul>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    POKE_API_URL: 'https://pokeapi.co/api/v2/',
+  },
   buildModules: ['@nuxtjs/eslint-module'],
   build: {
     postcss: {
